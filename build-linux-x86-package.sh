9c30469a2530897b7bcba562d7ecfea1fde65dde
kgraefe <>
2009-05-30 16:43:54 +0000

    * Version 1.5 - second edition (or so) :p

--- build-linux-x86-package.sh
+++ build-linux-x86-package.sh
@@ -6,6 +6,8 @@ DIR=${PROJECT}-`cat VERSION`-linux-x86 && \
 mkdir -p ${DIR} && \
 cp src/.libs/${PROJECT}.so ${DIR} && \
 cp po/de.gmo ${DIR}/${PROJECT}.mo && \
+cp ChangeLog ${DIR}/ChangeLog && \
+cp README.linux_binary ${DIR}/ReadMe && \
 cp install_linux_package.sh ${DIR}/install.sh && \
 tar czvf ${DIR}.tar.gz ${DIR} && \
 rm -rf ${DIR}
