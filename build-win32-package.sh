99f365de432e275f49af260400843def8e49cd45
kgraefe <>
2009-05-30 16:29:02 +0000

    * Version 1.5

--- build-win32-package.sh
+++ build-win32-package.sh
@@ -5,7 +5,8 @@ PROJECT=extended_blist_sort && \
 WIN32DIR=${PROJECT}-`cat VERSION`-win32 && \
 mkdir -p ${WIN32DIR}/pidgin/plugins && \
 mkdir -p ${WIN32DIR}/pidgin/locale/de/LC_MESSAGES && \
-sed 's/$/\r/' ChangeLog >${WIN32DIR}/ChangeLog && \
+sed 's/$/\r/' ChangeLog >${WIN32DIR}/ChangeLog.txt && \
+sed 's/$/\r/' README.win32 >${WIN32DIR}/ReadMe.txt && \
 cp src/${PROJECT}.dll ${WIN32DIR}/pidgin/plugins && \
 i586-mingw32msvc-strip --strip-unneeded ${WIN32DIR}/pidgin/plugins/${PROJECT}.dll && \
 cp po/de.gmo ${WIN32DIR}/pidgin/locale/de/LC_MESSAGES/${PROJECT}.mo && \
