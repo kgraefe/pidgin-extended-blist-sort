994aa56a04d60d66d077383923990b83e0949ef4
kgraefe <>
2010-03-16 16:01:23 +0000

    * debianized

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 aclocal \
 && automake --add-missing \
 && autoconf \
+&& libtoolize --copy --install \
 && (intltoolize --version) < /dev/null > /dev/null 2>&1 || {
     echo;
     echo "You must have intltool installed to compile extended-blist-sort";
