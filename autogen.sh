6c78755cba1ee989a4d91b0adfe370f77b65a750
Konrad Gräfe <konradgraefe@aol.com>
2016-01-20 12:26:22 -0800

    Fix autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,27 +1,23 @@
 #! /bin/sh
+set +x
 
-ls VERSION >/dev/null || exit 1
-ls configure.ac.in >/dev/null || exit 1
-ls COPYING >/dev/null || exit 1
-echo generating configure.ac || exit 1
+test -f VERSION || exit 1
+test -f configure.ac.in || exit 1
+test -f COPYING || exit 1
+
 
 languages=""
 for f in po/*.po
-do languages="$languages $(basename $f .po)"
+do test -f $f && languages="$languages $(basename $f .po)"
 done
 
-sed -e "s/@@VERSION@@/$(cat VERSION)/" -e "s/@@LANGUAGES@@/$(echo $languages)/" configure.ac.in >configure.ac &&
-echo aclocal || exit 1
-aclocal || exit 1
-echo autoheader || exit 1
-autoheader || exit 1
-echo libtoolize --copy || exit 1
-libtoolize --copy || exit 1
-echo automake --add-missing --copy || exit 1
-automake --add-missing --copy || exit 1
-echo autoconf || exit 1
-autoconf || exit 1
-echo libtoolize --copy --install || exit 1
-libtoolize --copy --install || exit 1
-echo intltoolize --copy --force || exit 1
-intltoolize --copy --force 
+
+sed \
+	-e "s/@@VERSION@@/$(cat VERSION)/" \
+	-e "s/@@LANGUAGES@@/$(echo $languages)/" \
+configure.ac.in >configure.ac || exit 1
+
+mkdir -p m4
+intltoolize --force --copy --automake || exit 1
+autoreconf --force --install --verbose || exit 1
+
