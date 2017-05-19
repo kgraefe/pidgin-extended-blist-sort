f62f8ea217f8c684b91ccc85d10b196f953c3195
Konrad Gräfe <konradgraefe@aol.com>
2017-05-19 10:57:34 +0200

    autogen.sh: Auto-generate shipped header files
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,13 @@
 set +x
 
 test -f COPYING || exit 1
+test -f Makefile.am.in || exit 1
 
 ./scripts/gen-configure_ac.sh || exit 1
+
+sed \
+	-e "s#@@HEADERFILES@@#$(find src -type f -name '*.h' | xargs echo)#" \
+Makefile.am.in >Makefile.am || exit 1
 
 mkdir -p m4
 intltoolize --force --copy --automake || exit 1
