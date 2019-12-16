50c8886fd3199c1e4aad4c2cbe36722931ed390c
Konrad Gräfe <konradgraefe@aol.com>
2019-12-16 13:46:10 +0100

    Update license headers
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- src/extended_blist_sort.c
+++ src/extended_blist_sort.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2008-2017 Konrad Gräfe <konradgraefe@aol.com>
+/* Copyright (C) 2008-2019 Konrad Gräfe <konradgraefe@aol.com>
  *
  * This software may be modified and distributed under the terms
  * of the GPL2.0 license. See the COPYING file for details.
